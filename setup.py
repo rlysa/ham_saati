71bc58723e16f3f4e7bd24bd31ca7f2d16c5c70f
tsuqueen <zerverweben@yandex.ru>
2025-03-16 13:47:43 +0300

    правка комментариев к коду

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
+"""
+Файл setup.py для сборки и установки пакета ham_saati.
+Использует setuptools для упаковки Flask-приложения.
+"""
+
 from setuptools import setup
 
 setup(
     name="ham_saati",
     version="0.1",
-    py_modules=["main"],  # Подключаем main.py
-    include_package_data=True,  # Включает файлы статики и шаблонов
+    py_modules=["main"],
+    include_package_data=True,  #включает файлы статики и шаблонов
     install_requires=[
         line.strip() for line in open("requirements.txt") if line.strip()
     ],
