c5abcb1739574437d6dbaaf811ea297b858ae7f1
rlysa <94913980+rlysa@users.noreply.github.com>
2025-04-14 07:58:58 +0300

    add main part, input data

--- saati_algorithm.py
+++ saati_algorithm.py
@@ -50,3 +50,46 @@ def oksv_and_novp(matrix):
     print(f'''Оценки компонента собственного вектора: {[round(i, 2) for i in oksv]}
 Нормализованные оценки вектора приоритета {[round(i, 4) for i in novp]}\n''')
     return novp
+
+
+if __name__ == '__main__':
+    print('Введите количество критериев (больше одного)')
+    amount_of_kr = check_amount(input())
+    while not amount_of_kr:
+        print('Введено не допустимое значение. Ведите количество критериев (больше одного)')
+        amount_of_kr = check_amount(input())
+
+    print('Введите количество объектов для сравнения')
+    amount_of_obj = check_amount(input())
+    while not amount_of_obj:
+        print('Введено не допустимое значение. Ведите количество критериев (больше одного)')
+        amount_of_obj = check_amount(input())
+
+    names_kr = check_names(amount_of_kr, 'критерия', 'критерий')
+    matrix_kr = fill_matrix(amount_of_kr, names_kr, 'критерий', '')
+
+    print_matrix(matrix_kr)
+    novp_kr = oksv_and_novp(matrix_kr)
+
+    names_obj = check_names(amount_of_obj, 'объекта', 'объект')
+    all_novp = []
+
+    for i in range(amount_of_kr):
+        matrix_obj = fill_matrix(amount_of_obj, names_obj, 'объект', f'по критерию "{names_kr[i]}"')
+        print_matrix(matrix_obj)
+        novp_obj = oksv_and_novp(matrix_obj)
+        all_novp.append(novp_obj)
+
+    results = {}
+    for i in range(amount_of_obj):
+        n = 0
+        for j in range(amount_of_kr):
+            print(round(all_novp[j][i], 3), end=' ')
+            n += all_novp[j][i] * novp_kr[j]
+        print(round(n, 3))
+        results[f'{names_obj[i]}'] = n
+    print()
+    result = sorted(results, key=results.get)[::-1]
+    for i in result:
+        print(f'{i} - {round(results[i], 3)}')
+    print()
